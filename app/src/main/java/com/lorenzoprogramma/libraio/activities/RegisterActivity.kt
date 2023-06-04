ca40b16aebfdd01d53850dac26a7a7821d320670
Loree7 <lorenzoprogramma0@gmail.com>
2023-06-04 12:32:39 +0200

    Fix RegisterActivity

--- app/src/main/java/com/lorenzoprogramma/libraio/activities/RegisterActivity.kt
+++ app/src/main/java/com/lorenzoprogramma/libraio/activities/RegisterActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.lorenzoprogramma.libraio.MainActivity
 import com.lorenzoprogramma.libraio.databinding.ActivityLoginBinding
 import com.lorenzoprogramma.libraio.databinding.ActivityRegisterBinding
 
@@ -18,10 +19,10 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var nome: String = binding.registerNomeEditText.toString()
-        var cognome: String = binding.registerCognomeEditText.toString()
-        var usurname: String = binding.registerUsurnameEditText.toString()
-        var password: String = binding.registerPasswordEditText.toString()
+        var nome: String = binding.registerNomeEditText.text.toString()
+        var cognome: String = binding.registerCognomeEditText.text.toString()
+        var usurname: String = binding.registerUsurnameEditText.text.toString()
+        var password: String = binding.registerPasswordEditText.text.toString()
         binding.registerButton.setOnClickListener {
             userRegistration(nome, cognome, usurname, password)
         }
@@ -31,7 +32,7 @@ class RegisterActivity : AppCompatActivity() {
         //inserire nel DB i dati del nuovo utente registrato
 
         //apertura loginActivity e chiusura RegisterActivity
-        val intent = Intent(this, LoginActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         setResult(Activity.RESULT_OK, intent);
         startActivity(intent)
