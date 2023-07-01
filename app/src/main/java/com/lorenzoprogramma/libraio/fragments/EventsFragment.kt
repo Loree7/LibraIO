642fa8c1643ea46f7be42f05ed6b02285c13e369
LuxC <luciocampisi17@gmail.com>
2023-07-01 11:02:28 +0200

    Events

--- app/src/main/java/com/lorenzoprogramma/libraio/fragments/EventsFragment.kt
+++ app/src/main/java/com/lorenzoprogramma/libraio/fragments/EventsFragment.kt
@@ -39,15 +39,19 @@ class EventsFragment : Fragment() {
         EventsAdapter(data)
 
 
-        for(i in 1 until data.size)
-            addEvents(i) { events ->
-                println("size ${events.size}")
-                for (event in events) {
-                    println("Events: $event")
-                    data.add(event)
-                    adapter.notifyDataSetChanged()
+        getTitles { arrayOfTitles ->
+            if(arrayOfTitles.isEmpty()){
+                println("Vuoto")
+            }else{
+                for(title in arrayOfTitles){
+                    getEvents(title) {event ->
+                        data.add(event)
+                        adapter.notifyDataSetChanged()
+                    }
                 }
             }
+        }
+
 
 
         binding.eventsRecyclerView.setHasFixedSize(true)
@@ -59,37 +63,58 @@ class EventsFragment : Fragment() {
 
         return binding.root
         }
-
-    private fun addEvents(id: Int, callback: (ArrayList<Events>) -> Unit) {
-        val query = "select author, title, start_date, categorie from events where id = '$id';"
+    private fun getEvents(title : String, callback: (Events) -> Unit) {
+        val query = "select author, title, start_date, categorie from events where title = '$title';"
         ClientNetwork.retrofit.select(query).enqueue(
             object : retrofit2.Callback<JsonObject> {
                 override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
                     if (response.isSuccessful) {
                         val result = (response.body()?.get("queryset") as JsonArray)
-                        //callback(result)
                         println(result)
-                        val arrayOfEvents = ArrayList<Events>()
+                        val title = result[0].asJsonObject.get("title").asString
+                        val author = result[0].asJsonObject.get("author").asString
+                        val date = result[0].asJsonObject.get("start_date").asString
+                        val categorie = result[0].asJsonObject.get("categorie").asString
+                        val event = Events(title, author, date, categorie)
+                        callback(event)
+                    } else{
+                        println("PROBLEMI")
+                    }
+                }
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    Log.e("retrofit", "ERRORE: ${t.message}", t)
+                    println("Problem on Book request")
+                }
+            }
+        )
+    }
+    private fun getTitles(callback: (ArrayList<String>) -> Unit) {
+        val query = "select title from events;"
+        ClientNetwork.retrofit.select(query).enqueue(
+            object : retrofit2.Callback<JsonObject> {
+                override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                    if (response.isSuccessful) {
+                        val result = (response.body()?.get("queryset") as JsonArray)
+                        println(result)
+                        val arrayOfTitle = ArrayList<String>()
                         var completeCallbacks = 0
                         for (i in 0 until result.size()) {
                             val title = result[i].asJsonObject.get("title").asString
-                            val author = result[i].asJsonObject.get("author").asString
+                            //val author = result[i].asJsonObject.get("author").asString
                             //val cover = result[i].asJsonObject.get("cover_path").asString
-                            val date = result[i].asJsonObject.get("cover_path").asString
+                            //val date = result[i].asJsonObject.get("cover_path").asString
                             //val type = result[i].asJsonObject.get("type").asString
-                            val categorie = result[i].asJsonObject.get("type").asString
+                            //val categorie = result[i].asJsonObject.get("type").asString
 //-----------------------------------------------------
-                                val event = Events(title, author, date, categorie)
-                                println("Evento: $event")
-                                arrayOfEvents.add(event)
+                                arrayOfTitle.add(title)
                                 completeCallbacks++
 
                                 if (completeCallbacks == result.size()) {
-                                    callback(arrayOfEvents)
+                                    callback(arrayOfTitle)
                                 }
                             }
                         } else{
-                        println("PROBLEMI")
+                            println("PROBLEMI")
                         }
                 }
                 override fun onFailure(call: Call<JsonObject>, t: Throwable) {
